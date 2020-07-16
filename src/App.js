dde67e3f0b6001e3fedbf7ef7fb693e4751d40e2
nazimkazim <nazik_111@mail.ru>
2020-07-16 22:01:45 +0600

    add first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 
 function App() {
+
+  const [reveal, setReveal] = useState(false);
+
+  const called = () => {
+    setReveal(true);
+    console.log('cliked');
+  };
+
+
+
+
+  const cells = [
+    [{
+      icon: "home",
+      x: 0,
+      y: -160
+    },
+    {
+      icon: "home",
+      x: 0,
+      y: -320
+    },
+    {
+      icon: "home",
+      x: 0,
+      y: -480
+    }], [
+      {
+        icon: "mail",
+        x: 140,
+        y: -80
+      },
+      {
+        icon: "mail",
+        x: 280,
+        y: -160
+      },
+      {
+        icon: "mail",
+        x: 420,
+        y: -240
+      }
+    ],
+    [{
+      icon: "notifications",
+      x: 140,
+      y: 80
+    },
+    {
+      icon: "notifications",
+      x: 280,
+      y: 160
+    },
+    {
+      icon: "notifications",
+      x: 420,
+      y: 240
+    }
+    ],
+    [{
+      icon: "add_a_photo",
+      x: 0,
+      y: 160
+    },
+    {
+      icon: "add_a_photo",
+      x: 0,
+      y: 320
+    },
+    {
+      icon: "add_a_photo",
+      x: 0,
+      y: 480
+    }
+  ],
+    [{
+      icon: "videocam",
+      x: -140,
+      y: 80
+    },
+    {
+      icon: "videocam",
+      x: -280,
+      y: 160
+    },
+    {
+      icon: "videocam",
+      x: -420,
+      y: 240
+    }
+  ],
+    [
+      {
+      icon: "search",
+      x: -140,
+      y: -80
+    },
+    {
+      icon: "search",
+      x: -280,
+      y: -160
+    },
+    {
+      icon: "search",
+      x: -420,
+      y: -240
+    }
+  ]
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" onClick={ called }>
+        <div className="toggle" id="toggle">
+          <span className="material-icons" id="add">
+            add
+        </span>
+        </div>
+      </div>
+      <CSSTransition
+        in={ reveal }
+        timeout={ 200 }
+        classNames="alert"
+        unmountOnExit
+      >
+        <div className="menu">
+          { cells.map((item, index) => (
+            item.map((itm, index) => (
+              <div className="item" key={ index } style={ { transform: `translate(${itm.x}px, ${itm.y}px)` } }>
+                <a href="link">
+                  <i className="material-icons">{ itm.icon }</i>
+                </a>
+              </div>
+            ))
+          )) }
+        </div>
+      </CSSTransition>
     </div>
   );
 }
