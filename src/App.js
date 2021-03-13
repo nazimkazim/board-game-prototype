26f0148f0cee0107b50fa0c8494962d3537d72c8
nazimkazim <nazik_111@mail.ru>
2021-03-13 16:30:14 +0600

    add build layout

--- src/App.js
+++ src/App.js
@@ -1,146 +1,146 @@
 import React, { useState } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import styled from 'styled-components';
 import './App.css';
+const { v4: uuidv4 } = require('uuid');
+
+const _cells = [
+  {
+
+    gridArea: '1/ 1 / 2 / 2',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '1 / 2 / 2 / 3',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '1 / 3 / 2 / 4',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '1 / 4 / 2 / 5',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '1 / 5 / 2 / 6',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '2 / 5 / 3 / 6',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '3 / 5 / 4 / 6',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '4 / 5 / 5 / 6',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '5 / 5 / 6 / 6',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '5 / 4 / 6 / 5',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '5 / 3 / 6 / 4',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '5 / 2 / 6 / 3',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '5 / 1 / 6 / 2',
+    id: uuidv4(),
+    move: false
+  }, {
+    gridArea: '4 / 1 / 5 / 2',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '3 / 1 / 4 / 2',
+    id: uuidv4(),
+    move: false
+  }, {
+
+    gridArea: '2 / 1 / 3 / 2',
+    id: uuidv4(),
+    move: false
+  },
+];
+
+const Parent = styled.div`
+  position:relative;
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  grid-column-gap: 0px;
+  grid-row-gap: 0px;
+  justify-items: stretch;
+  align-items: stretch;
+  max-width:100vh;
+  margin: 0 auto;
+}
+`;
+
+const Cell = styled.div`
+  grid-area:${props => props.gridArea};
+  border:${props => props.move && `2px solid yellow`};
+  height:calc(100vh / 5); 
+  weight:calc(100vh / 5);
+  background-color: blue;
+`;
+
+const Button = styled.div`
+  position:absolute;
+  min-width:30px;
+  height:30px;
+  top:2px;
+  left:10px;
+  background-color:red;
+`;
 
 function App() {
 
-  const [reveal, setReveal] = useState(false);
+  const [cells, setCells] = useState(_cells);
+  const [position, setPosition] = useState(0);
 
-  const called = () => {
-    setReveal(true);
-    console.log('cliked');
+  const movePlayer = () => {
+    const randomNum = getRandomIntInclusive(1, 6);
+    const found = cells.filter((_, i) => i === randomNum);
   };
 
-
-
-
-  const cells = [
-    [{
-      icon: "home",
-      x: 0,
-      y: -160
-    },
-    {
-      icon: "home",
-      x: 0,
-      y: -320
-    },
-    {
-      icon: "home",
-      x: 0,
-      y: -480
-    }], [
-      {
-        icon: "mail",
-        x: 140,
-        y: -80
-      },
-      {
-        icon: "mail",
-        x: 280,
-        y: -160
-      },
-      {
-        icon: "mail",
-        x: 420,
-        y: -240
-      }
-    ],
-    [{
-      icon: "notifications",
-      x: 140,
-      y: 80
-    },
-    {
-      icon: "notifications",
-      x: 280,
-      y: 160
-    },
-    {
-      icon: "notifications",
-      x: 420,
-      y: 240
-    }
-    ],
-    [{
-      icon: "add_a_photo",
-      x: 0,
-      y: 160
-    },
-    {
-      icon: "add_a_photo",
-      x: 0,
-      y: 320
-    },
-    {
-      icon: "add_a_photo",
-      x: 0,
-      y: 480
-    }
-  ],
-    [{
-      icon: "videocam",
-      x: -140,
-      y: 80
-    },
-    {
-      icon: "videocam",
-      x: -280,
-      y: 160
-    },
-    {
-      icon: "videocam",
-      x: -420,
-      y: 240
-    }
-  ],
-    [
-      {
-      icon: "search",
-      x: -140,
-      y: -80
-    },
-    {
-      icon: "search",
-      x: -280,
-      y: -160
-    },
-    {
-      icon: "search",
-      x: -420,
-      y: -240
-    }
-  ]
-  ];
-
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+  }
   return (
-    <div className="App">
-      <div className="container" onClick={ called }>
-        <div className="toggle" id="toggle">
-          <span className="material-icons" id="add">
-            add
-        </span>
-        </div>
-      </div>
-      <CSSTransition
-        in={ reveal }
-        timeout={ 200 }
-        classNames="alert"
-        unmountOnExit
-      >
-        <div className="menu">
-          { cells.map((item, index) => (
-            item.map((itm, index) => (
-              <div className="item" key={ index } style={ { transform: `translate(${itm.x}px, ${itm.y}px)` } }>
-                <a href="link">
-                  <i className="material-icons">{ itm.icon }</i>
-                </a>
-              </div>
-            ))
-          )) }
-        </div>
-      </CSSTransition>
-    </div>
+    <>
+      <Button onClick={ movePlayer }>Button</Button>
+      <Parent>
+        { cells.map(item => (
+          <Cell key={ item.id } move={ item.move } gridArea={ item.gridArea }>data</Cell>
+        )) }
+      </Parent>
+    </>
   );
 }
 
